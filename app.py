b20bebfa99bc634fcdfcd88abdb032c219359a67
Hrvoje <hrvoje.bresic@gmail.com>
2022-04-29 22:40:19 +0200

    added dockerfiles, procfile and changed app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from detector import Detector
 from classifier import Classifier
 from calculator import Calculator
 from flask import Flask, render_template, request
+import os
 
 
 detector = Detector()
@@ -46,3 +47,8 @@ def calculate():
         result = { "result": "Unable to calculate" }
 
     return result
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
